e8168f766a049af804d1661aa0300e606bf58ddf
K-T1107 <241204@st.yoshida-g.ac.jp>
2025-01-30 13:18:02 +0900

    判定の実装、表示

--- Program.cs
+++ Program.cs
@@ -11,20 +11,53 @@ namespace poka
         static void Main(string[] args)
         {
             string[]numberStr = new string[4];
+            int []number = new int[4];
             for (int i = 0; i < 4; i++)
             {
                 Console.WriteLine($"{i+1}番目の数字を入力してください >");
                 numberStr[i] = Console.ReadLine();
 
-                int number;
-                bool result = int.TryParse(numberStr[i], out number);
+                bool result = int.TryParse(numberStr[i], out number[i]);
 
                 if (result == false)
                 {
                     Console.WriteLine("\nエラー　数値で入力しろ!");
                 }
             }
-            Console.WriteLine($"{numberStr[0]},{numberStr[1]},{numberStr[2]},{numberStr[3]}");
+
+            //ペアの判定
+            int count = 0;
+            for (int i = 0; i < number.Count() - 1; i++)
+            {
+                for (int j = i + 1; j < number.Count(); j++)
+                {
+                    if ((number[i] & 0xF) == (number[j] & 0xF))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            // 成立した役を返す
+            switch (count)
+            {
+                case 0:
+                    Console.WriteLine("\nノーペア");
+                    break;
+                case 1:
+                    Console.WriteLine("\nワンペア");
+                    break;
+                case 2:
+                    Console.WriteLine("\nツーペア");
+                    break;
+                case 3:
+                    Console.WriteLine("\nスリーカード");
+                    break;
+                case 6:
+                    Console.WriteLine("\nフォーカード");
+                    break;
+            }
+
             Console.ReadLine();
         }
     }
