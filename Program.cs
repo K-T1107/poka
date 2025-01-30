5c1d15da1c90b355516c68f6eb18ed4d3489a96f
K-T1107 <241204@st.yoshida-g.ac.jp>
2025-01-30 13:42:21 +0900

    繰り返しプレイできるようにしました～～～～～～～～～～～

--- Program.cs
+++ Program.cs
@@ -10,55 +10,98 @@ namespace poka
     {
         static void Main(string[] args)
         {
-            string[]numberStr = new string[4];
-            int []number = new int[4];
-            for (int i = 0; i < 4; i++)
+            while (true)
             {
-                Console.WriteLine($"{i+1}番目の数字を入力してください >");
-                numberStr[i] = Console.ReadLine();
+                string[] numberStr = new string[4];
+                int[] number = new int[4];
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.Write($"{i + 1}番目の数字を入力してください >");
+                    numberStr[i] = Console.ReadLine();
 
-                bool result = int.TryParse(numberStr[i], out number[i]);
+                    bool result = int.TryParse(numberStr[i], out number[i]);
 
-                if (result == false)
+                    if (result == false)
+                    {
+                        Console.WriteLine("\nエラー　数値で入力しろ!");
+                    }
+                }
+
+                //ペアの判定
+                int count = 0;
+                for (int i = 0; i < number.Count() - 1; i++)
+                {
+                    for (int j = i + 1; j < number.Count(); j++)
+                    {
+                        if ((number[i] & 0xF) == (number[j] & 0xF))
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                // 成立した役を返す
+                switch (count)
+                {
+                    case 0:
+                        Console.WriteLine("\nノーペア");
+                        break;
+                    case 1:
+                        Console.WriteLine("\nワンペア");
+                        break;
+                    case 2:
+                        Console.WriteLine("\nツーペア");
+                        break;
+                    case 3:
+                        Console.WriteLine("\nスリーカード");
+                        break;
+                    case 6:
+                        Console.WriteLine("\nフォーカード");
+                        break;
+                }
+
+                Console.WriteLine("もう一回やんの？おれヤダ");
+                Console.WriteLine("1.やらせてください, 2.帰る");
+                string answerStr = Console.ReadLine();
+                bool result1 = int.TryParse(answerStr, out int answer);
+
+                if (result1 == false)
                 {
                     Console.WriteLine("\nエラー　数値で入力しろ!");
                 }
-            }
 
-            //ペアの判定
-            int count = 0;
-            for (int i = 0; i < number.Count() - 1; i++)
-            {
-                for (int j = i + 1; j < number.Count(); j++)
+                if (answer == 1)
                 {
-                    if ((number[i] & 0xF) == (number[j] & 0xF))
+                    Console.WriteLine("マジでもっかいやんの？");
+                    Console.WriteLine("1.やらせてください, 2.帰る");
+                    string answer2Str = Console.ReadLine();
+                    bool result2 = int.TryParse(answer2Str, out int answer2);
+
+                    if (result2 == false)
                     {
-                        count++;
+                        Console.WriteLine("\nエラー　数値で入力しろ!");
+                    }
+
+                    if (answer2 == 1)
+                    {
+                        Console.WriteLine("チッ");
+                        Console.Read();
+                    }
+                    if (answer2 == 2)
+                    {
+                        Console.WriteLine("二度と来るな");
+                        Console.Read();
+                        break;
                     }
                 }
+                else if (answer == 2) 
+                {
+                    Console.WriteLine("二度と来るな");
+                    Console.Read();
+                    break;
+                }
+                Console.ReadLine();
             }
-
-            // 成立した役を返す
-            switch (count)
-            {
-                case 0:
-                    Console.WriteLine("\nノーペア");
-                    break;
-                case 1:
-                    Console.WriteLine("\nワンペア");
-                    break;
-                case 2:
-                    Console.WriteLine("\nツーペア");
-                    break;
-                case 3:
-                    Console.WriteLine("\nスリーカード");
-                    break;
-                case 6:
-                    Console.WriteLine("\nフォーカード");
-                    break;
-            }
-
-            Console.ReadLine();
         }
     }
 }
