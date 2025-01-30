13053cdc35823f558125bf247dcdd6127b428e67
K-T1107 <241204@st.yoshida-g.ac.jp>
2025-01-30 11:29:48 +0900

    1

--- Program.cs
+++ Program.cs
@@ -10,7 +10,21 @@ namespace poka
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ポー力ー");
+            string[]numberStr = new string[3];
+            for (int i = 0; i <=3; i++)
+            {
+                Console.WriteLine($"{i+1}番目の数字を入力してください >");
+                numberStr[i] = Console.ReadLine();
+
+                int number;
+                bool result = int.TryParse(numberStr[i], out number);
+
+                if (result == false)
+                {
+                    Console.WriteLine("\nエラー　数値で入力しろ!");
+                }
+            }
+            Console.WriteLine($"{numberStr[0]},{numberStr[1]},{numberStr[2]},{numberStr[3]}");
             Console.ReadLine();
         }
     }
